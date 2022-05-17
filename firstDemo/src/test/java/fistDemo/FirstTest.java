4b9c3c72721696703999edd349ee20739704d2dd
eug2345 <eugeniet20@gmail.com>
2022-05-17 13:55:26 +0100

    firstDemo

---
+++ firstDemo/src/test/java/fistDemo/FirstTest.java
@@ -0,0 +1,18 @@
+package fistDemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+	@Test
+	public void printFirstNme() {
+		
+		
+		String name = "Eugenie";
+		
+		name = "Eugenie";
+		
+	Assertions.assertEquals("Eugenie", name);	
+	}
+}
